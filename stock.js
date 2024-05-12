3e0d7a4e3cb710cfa85e458992f38909f5a4220a
Ivanpereyra3 <ivanpereyra3@gmail.com>
2024-05-12 19:07:15 -0300

    proyecto final pereyra

--- stock.js
+++ stock.js
@@ -1,5 +1,10 @@
 
 
+function mostrarResultado(resultado) {
+    document.getElementById('resultados').innerHTML = resultado;
+}
+
+
 function verUrgenciaCompra() {
     const comprar = parseInt(prompt("Ver urgencia de compra\n1- Excluyente\n2- Ocasional"));
     urgencia(comprar);
@@ -12,6 +17,10 @@ function verNombresPescados() {
 
 function verCantidadPescados() {
     cantidadPescado();
+}
+
+function VerSinStock() {
+    PescadoSinStock();
 }
 
 
@@ -29,17 +38,54 @@ function urgencia(comprar) {
 }
 
 function tipoDePescado(nombre) {
-    let pescado = pescados.find(pescado => pescado.nombre.toLowerCase() === nombre.toLowerCase());
+    let pescado = pescados.find(p => p.nombre.toLowerCase() === nombre.toLowerCase());
     if (pescado !== undefined) {
+        Swal.fire({
+            title: "Felicitaciones",
+            text: "Haz elegido " + nombre,
+            imageUrl: ImagenPescado(nombre), 
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: "Custom image"
+        });
         mostrarResultado(`El pescado que buscabas:<br>ID: ${pescado.id}<br>NOMBRE: ${pescado.nombre}<br>COMPRA: ${pescado.compra}`);
     } else {
+        Swal.fire({
+            title: "Lo siento",
+            text: "El nombre no es correcto",
+        });
         mostrarResultado("El nombre no es correcto");
+
     }
 }
 
+function ImagenPescado(nombrePescado) {
+    switch (nombrePescado.toLowerCase()) {
+        case 'boga':
+            return 'https://static.cotodigital3.com.ar/sitios/fotos/full/00017800/00017805.jpg?3.0.170';
+        case 'sabalo':
+            return 'https://acdn.mitiendanube.com/stores/001/219/229/products/sabalo-entero1-28111bdb95d6fde75816026891460222-640-0.jpg';
+        case 'dorado':
+            return 'https://i.pinimg.com/236x/55/9d/f3/559df36ca00e554dc512c38ea4edfc08.jpg';
+        case 'surubi':
+            return 'https://www.magyp.gob.ar/sitio/areas/pesca_continental/especies/surubi.jpg';
+       
+    }
+}
+
+
 function cantidadPescado() {
+    Swal.fire({
+        title: "Te restan",
+        text: `${pescados.length}` + " tipos de pescados",
+        icon: "success",
+    });
+    
     mostrarResultado(`Tipos de pescados restantes: ${pescados.length}`);
 }
+
+
+
 
 function mostrarResultado(resultado) {
     document.getElementById('resultados').innerHTML = resultado;
@@ -53,6 +99,29 @@ function mostrarTabla(datos) {
     table += '</table>';
     mostrarResultado(table);
 }
+
+function ObtenerSinStock() {
+    const SinStock = './user.json';
+    fetch(SinStock)
+        .then(SinStock => SinStock.json())
+        .then(datos => {
+            datos.forEach(faltante => {
+                document.getElementById("SinStock").innerHTML += `
+                <tr>
+                <td>${faltante.nombre}</td>
+            </tr>
+            <tr>
+                <td>${faltante.precioAnterior}</td>
+            </tr>`
+            });
+        })
+        .catch(error => console.log('Error al obtener los datos:'));
+}
+
+
+
+
+    ObtenerSinStock();
 
 localStorage.setItem("pescados1", JSON.stringify(pescados));
 
